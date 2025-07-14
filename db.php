390cf309ec84ce8ce0b785eb12d6db8e0ac04a2f
Neha Kumarii 123 <nknehakumariii810@gmail.com>
2025-07-14 21:27:35 +0500

    Add iris data support to database layer

--- db.php
+++ db.php
@@ -9,4 +9,19 @@ if (!$con) {
 
 // Optional: Set charset to utf8 for proper character handling
 mysqli_set_charset($con, "utf8");
+
+// 1. Add this new function (Python API URL)
+function getPythonAPIUrl() {
+    return 'http://localhost:5000';
+}
+
+// 2. Add this main function (Iris Data Saver)
+function saveIrisData($roll_number, $iris_data) {
+    global $con;
+    $iris_data = mysqli_real_escape_string($con, $iris_data);
+    $query = "UPDATE attendance SET iris_data = '$iris_data' WHERE roll_number = '$roll_number'";
+    return mysqli_query($con, $query);
+}
+
+
 ?>
